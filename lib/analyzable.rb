87be3c9d738e67b81ba0256999e38d2c08d2342e
Maximiliano Pombo <maxipombo@gmail.com>
2016-03-19 20:35:39 -0300

    Print report

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -3,5 +3,10 @@ module Analyzable
   def average_price(data)
     (data.inject(0) {|total, item| total += item.price.to_f} / data.length).round(2)
   end
-  
+
+  def print_report(products)
+    report = "Average Price: #{average_price(products)}\n"
+    report
+  end
+
 end
